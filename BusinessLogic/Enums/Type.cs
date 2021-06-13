c52fc8be21f0c2aeffbe343ffac67f3d4ff84c19
Mariya <mariyapavlova2001@mail.ru>
2021-06-14 01:09:53 +0400

    base

---
+++ BusinessLogic/Enums/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Enums
+{
+    public enum Type
+    {
+        Принят = 0,
+        Выполняется = 1,
+        Готов = 2,
+        Оплачен = 3
+    }
+}
